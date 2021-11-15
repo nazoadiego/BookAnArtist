c329dcbec9b453015133e1ef81c79bf331d001c1
Bernardo Balestrini <balestrinibernardo@gmail.com>
2021-11-15 17:31:15 +0100

    bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :offers, only: [] do
+    resources :bookings, only: [:create, :update]
+  end
+  resources :bookings, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

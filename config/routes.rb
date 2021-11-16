6b7a5de0548e749ad7ee573477ea32e9caa464dd
Bernardo Balestrini <balestrinibernardo@gmail.com>
2021-11-16 10:29:53 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :offers, only: [] do
+  resources :offers, only: [:index, :show, :create, :new] do
     resources :bookings, only: [:create, :update]
   end
   resources :bookings, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :offers, only: [:index, :show, :create, :new]
+
 end

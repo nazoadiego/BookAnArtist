51cb01ce84f648cdcb5d0d99e941329a225b9359
diegomarrufo <dmarrufo@ucm.es>
2022-01-03 19:01:11 +0100

    started a new dashboard, 2 pannel grid, added a new table from daisy ui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        oceanblack: {
+        100: "#d3d3d5",
+        200: "#a6a7ab",
+        300: "#7a7c82",
+        400: "#4d5058",
+        500: "#21242e",
+        600: "#1a1d25",
+        700: "#14161c",
+        800: "#0d0e12",
+        900: "#070709"
+        },
+      },
     },
   },
   plugins: [

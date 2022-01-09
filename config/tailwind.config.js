a375d5d2f680723decaa94874ef14ca9fe2930e9
diegomarrufo <dmarrufo@ucm.es>
2022-01-10 00:24:00 +0100

    bundle fix

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -30,6 +30,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('daisyui.d.ts')
+    require('daisyui')
   ],
 }
